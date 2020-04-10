8fb7912327db52d2a2a017a1bb34cecf7e8c45a8
Aaron Jacobson <alj@nym.hush.com>
2020-04-09 21:16:00 -0700

    I think I've gotten the 2016_reader working

--- 2016_reader.py
+++ 2016_reader.py
@@ -6,24 +6,25 @@ topLineToPrint = ["race", "district", "name", "party", "has_opponent", "elected_
                   "other_professional_experience",
                   "education", "community_service", "statement", "contact"]
 candidateList = []
-main_lines = 0
-with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
-    outputWriter = csv.writer(outputCsv, delimiter=",", quotechar="|")
+with open("2016_statements.tsv", 'w', newline='\n', encoding="utf8") as outputCsv:
+    outputWriter = csv.writer(outputCsv, delimiter="\t", quotechar="~")
     outputWriter.writerow(topLineToPrint)
 
     for entry in os.listdir(base_folder):
         print("------------------------------Working on " + entry)
+        numLines = 0
         with open(base_folder + "/" + entry, "r", encoding="utf8") as inputFile:
             numLines = len(list(inputFile))
+
+        with open(base_folder + "/" + entry, "r", encoding="utf8") as inputFile:
             currentLine = 0
             line = inputFile.readline()
             currentLine += 1
             while currentLine < numLines:
-                print("On " + str(currentLine) + "/" + str(numLines))
-                main_lines += 1
+                # print("On " + str(currentLine) + "/" + str(numLines))
 
-                if line.count("|") > 0:
-                    print(line)
+                if line.count("|") > 1 and ("State Representative" in line or "State Senator" in line):
+                    print("-" + line)
                     # the start of a new race section
                     firstCandidateToPrint = []
                     secondCandidateToPrint = []
@@ -35,8 +36,10 @@ with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
                     if words[1 + indexOffset] == "Representative":
                         race = race + " " + words[5 + indexOffset] + " " + words[6 + indexOffset]
                     district = words[4 + indexOffset]
-                    firstCandidateToPrint.append([race, district])
-                    secondCandidateToPrint = firstCandidateToPrint
+                    firstCandidateToPrint.append(race.replace("\n", ""))
+                    firstCandidateToPrint.append(district.replace("\n", ""))
+                    secondCandidateToPrint.append(race.replace("\n", ""))
+                    secondCandidateToPrint.append(district.replace("\n", ""))
 
                     # now we deal with the candidates individually
                     name_1 = inputFile.readline() + " " + inputFile.readline()
@@ -44,23 +47,23 @@ with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
                     currentLine += 1
                     party_1 = inputFile.readline()
                     currentLine += 1
-                    firstCandidateToPrint.append(name_1)
-                    firstCandidateToPrint.append(party_1)
+                    firstCandidateToPrint.append(name_1.replace("\n", ""))
+                    firstCandidateToPrint.append(party_1.replace("\n", ""))
                     name_2 = inputFile.readline()
                     currentLine += 1
-                    if name_2 == "Unopposed":
+                    if "Unopposed" in name_2:
                         # no other candidate
-                        firstCandidateToPrint.append("no_opponent")
+                        firstCandidateToPrint.append("no_opponent".replace("\n", ""))
                         pass
                     else:
-                        name_2 = name_2 + inputFile.readline()
+                        name_2 = name_2 + " " + inputFile.readline()
                         currentLine += 1
                         party_2 = inputFile.readline()
                         currentLine += 1
-                        secondCandidateToPrint.append(name_2)
-                        secondCandidateToPrint.append(party_2)
-                        firstCandidateToPrint.append("has_opponent")
-                        secondCandidateToPrint.append("has_opponent")
+                        secondCandidateToPrint.append(name_2.replace("\n", ""))
+                        secondCandidateToPrint.append(party_2.replace("\n", ""))
+                        firstCandidateToPrint.append("has_opponent".replace("\n", ""))
+                        secondCandidateToPrint.append("has_opponent".replace("\n", ""))
 
                     inputFile.readline()  # get rid of "Elected Experience" line
                     currentLine += 1
@@ -68,49 +71,49 @@ with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
                     line = inputFile.readline()  # move onto the first line after "Elected Experience"
                     currentLine += 1
                     elected_experience_1 = ""
-                    while line and line != "Other Professional Experience":
+                    while line and "Other Professional Experience" not in line:
                         elected_experience_1 = elected_experience_1 + " " + line
                         line = inputFile.readline()
                         currentLine += 1
-                    firstCandidateToPrint.append(elected_experience_1)
+                    firstCandidateToPrint.append(elected_experience_1.replace("\n", ""))
 
                     line = inputFile.readline()  # move onto the first line after "Other Professional Experience"
                     currentLine += 1
                     other_professional_experience_1 = ""
-                    while line and line != "Education":
+                    while line and "Education" not in line:
                         other_professional_experience_1 = other_professional_experience_1 + " " + line
                         line = inputFile.readline()
                         currentLine += 1
-                    firstCandidateToPrint.append(other_professional_experience_1)
+                    firstCandidateToPrint.append(other_professional_experience_1.replace("\n", ""))
 
                     line = inputFile.readline()  # move onto first line after "Education"
                     currentLine += 1
                     education_1 = ""
-                    while line and line != "Community Service":
+                    while line and "Community Service" not in line:
                         education_1 = education_1 + " " + line
                         line = inputFile.readline()
                         currentLine += 1
-                    firstCandidateToPrint.append(education_1)
+                    firstCandidateToPrint.append(education_1.replace("\n", ""))
 
                     line = inputFile.readline()  # move onto the first line after "Community Service"
                     currentLine += 1
                     community_service_1 = ""
-                    while line and line != "Statement":
+                    while line and "Statement" not in line:
                         community_service_1 = community_service_1 + " " + line
                         line = inputFile.readline()
                         currentLine += 1
-                    firstCandidateToPrint.append(community_service_1)
+                    firstCandidateToPrint.append(community_service_1.replace("\n", ""))
 
                     line = inputFile.readline()  # move onto the first line after "Statement"
                     currentLine += 1
                     statement_1 = ""
-                    while line and line != "Contact":
+                    while line and "Contact" not in line:
                         statement_1 = statement_1 + " " + line
                         line = inputFile.readline()
                         currentLine += 1
-                    firstCandidateToPrint.append(statement_1)
+                    firstCandidateToPrint.append(statement_1.replace("\n", ""))
 
-                    if name_2 == "Unopposed":
+                    if "Unopposed" in name_2:
                         line = inputFile.readline()  # move onto the first line after "Contact"
                         currentLine += 1
                         contact_1 = ""
@@ -118,62 +121,62 @@ with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
                             contact_1 = contact_1 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        firstCandidateToPrint.append(contact_1)
+                        firstCandidateToPrint.append(contact_1.replace("\n", ""))
                     else:
                         line = inputFile.readline()  # move onto the first line after "Contact"
                         currentLine += 1
                         contact_1 = ""
-                        while line and line != "Elected Experience":
+                        while line and "Elected Experience" not in line:
                             contact_1 = contact_1 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        firstCandidateToPrint.append(contact_1)
+                        firstCandidateToPrint.append(contact_1.replace("\n", ""))
 
                         # now onto the second candidate
                         line = inputFile.readline()  # move onto the first line after "Elected Experience"
                         currentLine += 1
                         elected_experience_2 = ""
-                        while line and line != "Other Professional Experience":
+                        while line and "Other Professional Experience" not in line:
                             elected_experience_2 = elected_experience_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(elected_experience_2)
+                        secondCandidateToPrint.append(elected_experience_2.replace("\n", ""))
 
                         line = inputFile.readline()  # move onto the first line after "Other Professional Experience"
                         currentLine += 1
                         other_professional_experience_2 = ""
-                        while line and line != "Education":
+                        while line and "Education" not in line:
                             other_professional_experience_2 = other_professional_experience_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(other_professional_experience_2)
+                        secondCandidateToPrint.append(other_professional_experience_2.replace("\n", ""))
 
                         line = inputFile.readline()  # move onto the first line after "Education"
                         currentLine += 1
                         education_2 = ""
-                        while line and line != "Community Service":
+                        while line and "Community Service" not in line:
                             education_2 = education_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(education_2)
+                        secondCandidateToPrint.append(education_2.replace("\n", ""))
 
                         line = inputFile.readline()  # move onto the first line after "Community Service"
                         currentLine += 1
                         community_service_2 = ""
-                        while line and line != "Statement":
+                        while line and "Statement" not in line:
                             community_service_2 = community_service_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(community_service_2)
+                        secondCandidateToPrint.append(community_service_2.replace("\n", ""))
 
                         line = inputFile.readline()  # move onto the first line after "Statement"
                         currentLine += 1
                         statement_2 = ""
-                        while line and line != "Contact":
+                        while line and "Contact" not in line:
                             statement_2 = statement_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(statement_2)
+                        secondCandidateToPrint.append(statement_2.replace("\n", ""))
 
                         line = inputFile.readline()  # move onto the first lane after "Contact"
                         currentLine += 1
@@ -182,21 +185,21 @@ with open("2016_statements.csv", 'w', newline='', encoding="utf8") as outputCsv:
                             contact_2 = contact_2 + " " + line
                             line = inputFile.readline()
                             currentLine += 1
-                        secondCandidateToPrint.append(contact_2)
+                        secondCandidateToPrint.append(contact_2.replace("\n", ""))
                     # write both candidates
                     if name_1 not in candidateList:
                         print("--------------------------------Found " + name_1 + " for " + race + " in " + district)
-                        candidateList.append(name_1)
+                        candidateList.append(name_1.replace("\n", ""))
                         outputWriter.writerow(firstCandidateToPrint)
-                    if name_2 not in candidateList:
+                    if name_2 not in candidateList and "Unopposed" not in name_2:
                         print("--------------------------------Found " + name_2 + " for " + race + " in " + district)
-                        candidateList.append(name_2)
+                        candidateList.append(name_2.replace("\n", ""))
                         outputWriter.writerow(secondCandidateToPrint)
+
                 else:
                     line = inputFile.readline()
                     currentLine += 1
-            pass
-        break
+
         pass
-    #outputWriter.writerow(["hello world"])
+
     pass
